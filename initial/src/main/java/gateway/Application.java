846ebc31a7d2c28923d34b0f369c6fb24bc124a0
lkssgn <95890624+lkssgn@users.noreply.github.com>
2022-01-11 19:21:41 +0530

    Circuit Breaker with fallback

--- initial/src/main/java/gateway/Application.java
+++ initial/src/main/java/gateway/Application.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Mono;
+
+@RestController
 @SpringBootApplication
 public class Application {
 
@@ -13,13 +18,26 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 	
+	private static final String HTTP_BIN = "http://httpbin.org";
 
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(p->p.path("/get")
+				.route(p->p
+						.path("/get")
 						.filters(f->f.addRequestHeader("Header", "Value"))
-						.uri("http://httpbin.org:80"))
+						.uri(HTTP_BIN))
+				.route(p->p
+						.host("*.circuitbreaker.com")
+						.filters(f->f.circuitBreaker(config->config
+								.setName("mycmd")
+								.setFallbackUri("forward:/fallback")))
+						.uri(HTTP_BIN))
 				.build();
+	}
+	
+	@RequestMapping("/fallback")
+	public Mono<String> fallback(){
+		return Mono.just("fallback");
 	}
 }
